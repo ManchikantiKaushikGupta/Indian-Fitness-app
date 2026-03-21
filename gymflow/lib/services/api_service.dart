32d79fb9d961d65b453aaa39189a4ef433a25fcf
MANCHIKANTI KAUSHIK GUPTA <167305411+ManchikantiKaushikGupta@users.noreply.github.com>
2026-03-21 22:25:48 +0530

    ..

--- gymflow/lib/services/api_service.dart
+++ gymflow/lib/services/api_service.dart
@@ -3,8 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
   final Dio _dio = Dio();
-  // Using 10.0.2.2 for Android emulator, change to localhost for web/iOS, or IP for physical device
-  final String baseUrl = 'http://10.0.2.2:3000';
+  // Using localhost for Web testing
+  final String baseUrl = 'http://localhost:3000';
 
   ApiService() {
     _dio.options.baseUrl = baseUrl;
